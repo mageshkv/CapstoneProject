5a8a61112c9e336e72cde547dadf5a9f023c3fa3
lava1984 <k_v_mahesh@rediffmail.com>
2023-04-27 19:27:54 +0530

    test

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1 style='text-align: center;'>Hello World,My name is Sshreya!</h1>"
+    return "<h1 style='text-align: center;'>Hello ! My capstone project - Lavanya!</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
